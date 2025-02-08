b0e5772d5b6bfa63e99bb3ddeed4cc6b09d7f0be
Andrew Birmingham <andrew_birmingham@msn.com>
2025-02-08 14:59:05 -0700

    pushing weather routes partially done

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,13 +1,29 @@
 import { Router, type Request, type Response } from 'express';
 const router = Router();
-// import request from 'request';
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
+
 // TODO: POST Request with city name to retrieve weather data
 router.post('/', async (req: Request, res: Response) => {
+  const { cityName } = req.body;
+  if(cityName) {
+    try{
+      const weatherData = await WeatherService.getWeatherByCity(cityName);
+
+      await WeatherService.addCityToHistory(cityName);
+
+      return res.status(200).json(weatherData);
+    } catch (error)
+    console.error(error);
+    return res.status(500).json({ message: 'Error getting weather data'})
+  } else {
+    return res.status(400).json({ message: 'The city name is required'})
+  }
+  
+  
+
   // TODO: GET weather data from city name
-  const cityName = req.body.cityName;
   WeatherService.getWeatherForCity(cityName).then((data) =>{
     // fire the historyservice add city method and pass the city name to it
     res.json(data) //returning weather data from the backend to the frontend
@@ -41,13 +57,23 @@ router.get('/history', async (_req: Request, res: Response) => {
     const savedCities = await HistoryService.getCities();
     res.json(savedCities);
   } catch (err){
-    
-  }
+    console.log(err);
+    res.status(500).json(err);
+    }
 });
 
 // * BONUS TODO: DELETE city from search history
-router.delete('/history/:id', async (_req: Request, _res: Response) => {
-  
+router.delete('/history/:id', async (req: Request, res: Response) => {
+  try{
+    if(!req.params.id) {
+      res.status(400).json({ msg: 'City id is required'});
+    }
+    await HistoryService.removeCity(req.params.id);
+    res.json({ success: 'City has been removed successfully'})
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 export default router;
